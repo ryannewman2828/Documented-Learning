9c93fb1ee212f60fc5ae31da54d950cddee59dc3
Ryan Newman <ryannewman2828@gmail.com>
2016-11-29 11:54:50 -0500

    Atkin bound checking

--- Algorithms/Computational Mathematics/Sieve of Atkin/SieveOfAtkin.py
+++ Algorithms/Computational Mathematics/Sieve of Atkin/SieveOfAtkin.py
@@ -4,39 +4,48 @@
 # TODO make work
 
 limit = int(input("Enter the limit for the primes you want to find: "))
-arr = [False] * limit
-arr[2] = True
-arr[3] = True
-arr[5] = True
+arr = [False] * (limit + 1)
+if limit <= 1:
+    exit()
+elif limit == 2:
+    print(2)
+    exit()
+elif limit <= 4:
+    print(2 + "\n" + 3)
+    exit()
+
+arr[2] = arr[3] = arr[5] = True
 list1 = [1, 13, 17, 29, 37, 41, 49, 53]
 list2 = [7, 19, 31, 43]
 list3 = [11, 23, 47, 59]
 x = 1
 y = 1
 
-while (x * x < limit):
-    while (y * y < limit):
+while x * x < limit:
+    while y * y < limit:
         n = 4 * x * x + y * y
-        if (n <= limit and n % 60 in list1):
+        if n <= limit and n % 60 in list1:
             arr[n] = not arr[n]
 
         n = 3 * x * x + y * y
-        if (n <= limit and n % 60 in list2):
+        if n <= limit and n % 60 in list2:
             arr[n] = not arr[n]
 
         n = 3 * x * x - y * y
-        if (n <= limit and n % 60 in list3):
+        if n <= limit and n % 60 in list3:
             arr[n] = not arr[n]
         y += 1
     x += 1
+
 x = 7
-while (x * x < limit):
+while x * x < limit:
     if arr[x]:
         y = x * x
-        while(y < limit):
+        while y < limit:
             arr[y] = False
             y += x * x
+    x += 1
 
-for i in arr:
+for i in range(2, limit + 1):
     if arr[i]:
         print(i)
