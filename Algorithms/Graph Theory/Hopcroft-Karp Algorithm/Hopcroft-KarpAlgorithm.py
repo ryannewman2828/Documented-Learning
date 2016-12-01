1250d7abe0104f5b5257448dd8c35b6615d10d3a
Ryan Newman <ryannewman2828@gmail.com>
2016-11-30 21:07:10 -0500

    Finished Hopcrost-Karp setup

--- Algorithms/Graph Theory/Hopcroft-Karp Algorithm/Hopcroft-KarpAlgorithm.py
+++ Algorithms/Graph Theory/Hopcroft-Karp Algorithm/Hopcroft-KarpAlgorithm.py
@@ -1,43 +1,70 @@
 #!bin/python
 from functools import reduce
 
+
 # program assumes valid input
-
 class Vertex:
-    name = ""
-    edges = []
-    saturated = False
-
     def __init__ (self, name, saturated):
         self.name = name
         self.saturated = saturated
+        self.neighbours = []
 
-    def addEdge (self, edge):
-        self.edges.append(edge)
+    def addNeighbour (self, vertex):
+        self.neighbours.append(vertex)
 
-class Edge:
-    def __init__(self, vertexA, vertexB):
-        self.vertexA = vertexA
-        self.vertexB = vertexB
+
+def setMinus(set1, set2):
+    return list(filter((lambda x: x not in set2), set1))
 
 VERTICE_FILE_NAME = "vertice.txt"
 EDGES_FILE_NAME = "edge.txt"
 MATCHING_FILE_NAME = "matching.txt"
 
+# init list
 vertices = [line.rstrip('\n') for line in open(VERTICE_FILE_NAME)]
 edges = [line.rstrip('\n') for line in open(EDGES_FILE_NAME)]
 matches = [line.rstrip('\n') for line in open(MATCHING_FILE_NAME)]
 
+# convert to tuples
 edges = list(map((lambda x: tuple(x)), edges))
 matches = list(map((lambda x: tuple(x)), matches))
 
-vertices = list(map(
-    (lambda x: Vertex(
-        x,
-        reduce((lambda y, z: y or z[0] == x or z[1] == x), matches))),
-    vertices))
+# create bipartition
+#todo optimize this
+A = [vertices[0]]
+B = []
+for e in edges:
+    if e[0] in A and e[1] not in B:
+        B.append(e[1])
+    elif e[0] in B and e[1] not in A:
+        A.append(e[1])
+    elif e[1] in A and e[0] not in B:
+        B.append(e[0])
+    elif e[1] in B and e[0] not in A:
+        A.append(e[0])
+    elif e[0] not in A and B and e[1] not in A and B:
+        # cant determine so move to the back
+        edges.append(e)
+
+#init vertices
+verticesA = dict((
+    v, Vertex(
+        v, reduce((lambda y, z: y or z[0] == v or z[1] == v), [False] + matches))) for v in A)
+
+verticesB = dict((
+    v, Vertex(
+        v, reduce((lambda y, z: y or z[0] == v or z[1] == v), [False] + matches))) for v in B)
 
 for e in edges:
-    for v in vertices:
-        if v.name == e[0] or v.name == e[1]:
-            v.addEdge(e)
+    if e[0] in verticesA:
+        verticesA[e[0]].addNeighbour(verticesB[e[1]])
+        verticesB[e[1]].addNeighbour(verticesA[e[0]])
+    if e[0] in verticesB:
+        verticesB[e[0]].addNeighbour(verticesA[e[1]])
+        verticesA[e[1]].addNeighbour(verticesB[e[0]])
+
+X = list(filter((lambda x: not x[1].saturated), verticesA.items()))
+Y = []
+
+#while True:
+
