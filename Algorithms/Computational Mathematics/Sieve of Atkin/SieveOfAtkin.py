6db7f72dbc94b5b71987493426b1a4cacd497127
Ryan Newman <ryannewman2828@gmail.com>
2016-12-01 12:13:33 -0500

    Working Atkin
    The Atkin is now 'working' but needs to be heavily optimized

--- Algorithms/Computational Mathematics/Sieve of Atkin/SieveOfAtkin.py
+++ Algorithms/Computational Mathematics/Sieve of Atkin/SieveOfAtkin.py
@@ -1,51 +1,97 @@
 #!/usr/bin/python
 
 # TODO make more efficient
-# TODO make work
 
 limit = int(input("Enter the limit for the primes you want to find: "))
-arr = [False] * (limit + 1)
-if limit <= 1:
-    exit()
-elif limit == 2:
+arr = [True] * (limit + 61)
+
+if limit >= 2:
     print(2)
-    exit()
-elif limit <= 4:
-    print(2 + "\n" + 3)
+if limit >= 3:
+    print(3)
+if limit >= 5:
+    print(5)
+if limit < 5:
     exit()
 
-arr[2] = arr[3] = arr[5] = True
 list1 = [1, 13, 17, 29, 37, 41, 49, 53]
 list2 = [7, 19, 31, 43]
 list3 = [11, 23, 47, 59]
 x = 1
 y = 1
 
-while x * x < limit:
-    while y * y < limit:
+w = 0
+while w <= limit / 60:
+    for x in list1 + list2 + list3:
+        arr[60 * w + x] = False
+    w += 1
+
+x = 1
+y = 1
+n = 5
+while n <= limit:
+    while n <= limit:
+        if n % 60 in list1:
+            arr[n] = not arr[n]
+        y += 2
         n = 4 * x * x + y * y
-        if n <= limit and n % 60 in list1:
-            arr[n] = not arr[n]
+    x += 1
+    y = 1
+    n = 4 * x * x + y * y
 
+x = 1
+y = 2
+n = 7
+while n <= limit:
+    while n <= limit:
+        if n % 60 in list2:
+            arr[n] = not arr[n]
+        y += 2
         n = 3 * x * x + y * y
-        if n <= limit and n % 60 in list2:
-            arr[n] = not arr[n]
+    x += 2
+    y = 2
+    n = 3 * x * x + y * y
 
+x = 2
+y = x - 1
+n = 11
+while n <= limit:
+    while n <= limit and y >= 1:
+        if n % 60 in list3:
+            arr[n] = not arr[n]
+        y -= 2
         n = 3 * x * x - y * y
-        if n <= limit and n % 60 in list3:
-            arr[n] = not arr[n]
-        y += 1
     x += 1
+    y = x - 1
+    n = 3 * x * x - y * y
 
-x = 7
-while x * x < limit:
-    if arr[x]:
-        y = x * x
-        while y < limit:
-            arr[y] = False
-            y += x * x
-    x += 1
+# TODO optimize this
+w = 0
+n = 0
+while n * n <= limit:
+    for x in list1 + list2 + list3:
+        n = 60 * w + x
+        if n * n > limit:
+            break
+        if arr[n]:
+            c = 0
+            q = 0
+            while c <= limit:
+                for y in list1 + list2 + list3:
+                    c = n * n * (60 * w + y)
+                    if c <= limit:
+                        arr[c] = False
+                q += 1
+    w += 1
+    n = w * w
 
-for i in range(2, limit + 1):
-    if arr[i]:
-        print(i)
+n = 7
+w = 0
+primes = [2, 3, 5]
+while n <= limit:
+    for x in list1 + list2 + list3:
+        n = 60 * w + x
+        if n <= limit and arr[n]:
+            print(n)
+    w += 1
+
