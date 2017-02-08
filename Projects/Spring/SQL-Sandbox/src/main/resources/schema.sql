c2edb3fe4c26d6cf0be2b355f6733cc7426b5d1e
Ryan Newman <ryannewman2828@gmail.com>
2017-02-07 21:23:00 -0500

    Added more data

--- Projects/Spring/SQL-Sandbox/src/main/resources/schema.sql
+++ Projects/Spring/SQL-Sandbox/src/main/resources/schema.sql
@@ -4,6 +4,8 @@ CREATE TABLE IF NOT EXISTS Users
     id int NOT NULL AUTO_INCREMENT,
     name varchar(255) NOT NULL,
     email varchar(255) NOT NULL,
-    salary int,
+    salary int NOT NULL,
+    grade int CHECK (grade >= 0 && grade <= 100),
+    pass bit NOT NULL,
     PRIMARY KEY (id)
 );
