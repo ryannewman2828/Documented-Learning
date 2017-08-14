bea1a7d57e7b8c2e59fbf038b3b8c06669fdd239
Ryan Newman <ryannewman2828@gmail.com>
2017-08-13 22:39:39 -0400

    Project Euler Problem 41

---
+++ Project Euler/41-50/Problem_41.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+import itertools
+
+
+def tupToInt(num):
+    n = 0
+    for i in num:
+        n *= 10
+        n += i
+    return n
+
+def isPrime(num):
+    for i in range(2, int(num ** 0.5)):
+        if num // i == num / i:
+            return False
+    return True
+
+nums = [1]
+pans = [1]
+for i in range(2, 10):
+    nums.append(i)
+    pans.extend(list(itertools.permutations(nums)))
+for i in range(1, len(pans)):
+    pans[i] = tupToInt(pans[i])
+
+pans.sort()
+for i in range(len(pans) - 1, -1, -1):
+    if isPrime(pans[i]):
+        print(pans[i])
+        break
