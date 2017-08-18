53ea6c58c2692b0f5b7e1a4afefa3921beb6300c
Ryan Newman <ryannewman2828@gmail.com>
2017-08-17 21:55:15 -0400

    Project Euler Problem 58 temp

---
+++ Project Euler/51-60/Problem_58.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+import random
+
+def isPrime(n, k):
+    r = 0
+    d = n - 1
+    while d % 2 == 0:
+        d //= 2
+        r += 1
+    for _ in range(k):
+        a = random.randint(2, n - 2)
+        x = a ** d % n
+        if x == 1 or x == n - 1:
+            continue
+        cont = False
+        for __ in range(r - 1):
+            x = x ** 2 % n
+            if x == 1:
+                return False
+            if x == n - 1:
+                cont = True
+                break
+        if cont:
+            continue
+        return False
+    return True
+
+
+square = 3
+length = 2
+primes = 3
+while primes / (((square - 1) // 2) * 4) >= 0.1:
+    print(length)
+    square += 2
+    length += 2
+    for i in range(1, 4):
+        if isPrime(square ** 2 - length * i, 10):
+            primes += 1
+print(length)
