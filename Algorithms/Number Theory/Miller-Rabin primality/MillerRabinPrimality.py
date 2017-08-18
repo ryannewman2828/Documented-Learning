cc2309f42bead6e2825b3359688edbcc16e3e84f
Ryan Newman <ryannewman2828@gmail.com>
2017-08-17 22:10:45 -0400

    Miller-Rabin Optimization and Project Euler Problem 58

--- Algorithms/Number Theory/Miller-Rabin primality/MillerRabinPrimality.py
+++ Algorithms/Number Theory/Miller-Rabin primality/MillerRabinPrimality.py
@@ -6,16 +6,16 @@ def isPrime(n, k):
     r = 0
     d = n - 1
     while d % 2 == 0:
-        d //= 2
+        d >>= 1
         r += 1
     for _ in range(k):
         a = random.randint(2, n - 2)
-        x = a ** d % n
+        x = pow(a, d, n)
         if x == 1 or x == n - 1:
             continue
         cont = False
         for __ in range(r - 1):
-            x = x ** 2 % n
+            x = pow(x, 2, n)
             if x == 1:
                 return False
             if x == n - 1:
--- Project Euler/51-60/Problem_58.py
+++ Project Euler/51-60/Problem_58.py
@@ -5,16 +5,16 @@ def isPrime(n, k):
     r = 0
     d = n - 1
     while d % 2 == 0:
-        d //= 2
+        d >>= 1
         r += 1
     for _ in range(k):
         a = random.randint(2, n - 2)
-        x = a ** d % n
+        x = pow(a, d, n)
         if x == 1 or x == n - 1:
             continue
         cont = False
         for __ in range(r - 1):
-            x = x ** 2 % n
+            x = pow(x, 2, n)
             if x == 1:
                 return False
             if x == n - 1:
@@ -30,10 +30,9 @@ square = 3
 length = 2
 primes = 3
 while primes / (((square - 1) // 2) * 4) >= 0.1:
-    print(length)
     square += 2
     length += 2
     for i in range(1, 4):
-        if isPrime(square ** 2 - length * i, 10):
+        if isPrime(pow(square, 2) - length * i, 10):
             primes += 1
-print(length)
+print(length - 1)
