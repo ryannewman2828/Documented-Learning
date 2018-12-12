b315c731eacc831b7145cae4a1f2c5f03b209443
Ryan Newman <ryannewman2828@gmail.com>
2018-12-12 18:35:45 -0500

    Three sum (without comment)

---
+++ LeetCode/Algorithms/ThreeSum_15.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class ThreeSum_15 {
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> solutions = new ArrayList<>();
+        List<Integer> numbers = new ArrayList<>();
+        for (int num : nums) {
+            numbers.add(num);
+        }
+        Collections.sort(numbers);
+
+        for (int i = 0; i < numbers.size(); i++) {
+            if (i > 0 && numbers.get(i).equals(numbers.get(i - 1))) continue;
+            List<List<Integer>> ans = twoSum(numbers, -numbers.get(i), i);
+            for (List<Integer> a: ans) {
+
+                List<Integer> solution = new ArrayList<>();
+
+                solution.add(a.get(0));
+                solution.add(a.get(1));
+                solution.add(numbers.get(i));
+
+                solutions.add(solution);
+            }
+        }
+        return solutions;
+    }
+
+    private static List<List<Integer>> twoSum(List<Integer> nums, int target, int forbidIndex) {
+        Map<Integer, Integer> lookup = new HashMap<>();
+        Set<List<Integer>> solutions = new HashSet<>();
+        for (int i = forbidIndex + 1; i < nums.size(); i++) {
+            if (lookup.containsKey(target - nums.get(i))) {
+                List<Integer> solution = new ArrayList<>();
+                solution.add(nums.get(i));
+                solution.add(nums.get(lookup.get(target - nums.get(i))));
+                solutions.add(solution);
+            }
+            lookup.put(nums.get(i), i);
+        }
+        return new ArrayList(solutions);
+    }
+}
